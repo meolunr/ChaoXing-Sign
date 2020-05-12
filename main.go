6dd2dc8465dfff5f9b6e8c98dec24d2cde4f1f6e
meolunr <meolunr@gmail.com>
2020-05-12 22:17:34 +0800

    Get course list

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 	loadProfile()
 	newHttpClient()
 	login(profile.Username, profile.Password)
+	//getCourses()
 }
 
 func loadProfile() {
@@ -59,6 +60,17 @@ func login(username string, password string) {
 	if jsonResp.Status == true {
 		fmt.Println("用户登录成功")
 	}
+}
+
+func getCourses() {
+	request, _ := http.NewRequest(http.MethodGet, "https://mooc1-api.chaoxing.com/mycourse/backclazzdata", nil)
+	request.Header.Add("User-Agent", "Dalvik/2.1.0 (Linux; U; Android 10; Pixel 2) com.chaoxing.mobile/ChaoXingStudy_3_4.3.7_android_phone_497_27 (@Kalimdor)_aed7e7f96119453a9c9727776a940d5e")
+
+	response, _ := client.Do(request)
+	defer bodyClose(response.Body)
+	contentBytes, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(contentBytes))
 }
 
 func bodyClose(body io.Closer) {
