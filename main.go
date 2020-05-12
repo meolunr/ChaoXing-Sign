2fc4d748e0d957ee7b7bc92e7f733d13e44dab0f
meolunr <meolunr@gmail.com>
2020-05-12 18:47:15 +0800

    Login Implementation

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
 
 func main() {
-	fmt.Println("Hello World")
+}
+
+func login(username string, password string) {
+	loginUrl, _ := url.Parse("https://passport2-api.chaoxing.com/v11/loginregister")
+	params := url.Values{}
+	params.Set("uname", username)
+	params.Set("code", password)
+	loginUrl.RawQuery = params.Encode()
+
+	request, _ := http.NewRequest("POST", loginUrl.String(), nil)
+	request.Header.Add("User-Agent", "Dalvik/2.1.0 (Linux; U; Android 10; Pixel 2) com.chaoxing.mobile/ChaoXingStudy_3_4.3.7_android_phone_497_27 (@Kalimdor)_aed7e7f96119453a9c9727776a940d5e")
+
+	client := http.Client{}
+	response, _ := client.Do(request)
+	defer bodyClose(response.Body)
+	contentBytes, _ := ioutil.ReadAll(response.Body)
+
+	jsonResp := &Response{}
+	_ = json.Unmarshal(contentBytes, jsonResp)
+
+	fmt.Println(jsonResp.Message)
+}
+
+func bodyClose(body io.Closer) {
+	_ = body.Close()
+}
+
+type Response struct {
+	Message string `json:"mes"`
+	Status  bool   `json:"status"`
 }
